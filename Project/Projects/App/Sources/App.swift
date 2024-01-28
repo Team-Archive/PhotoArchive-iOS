596382f15ac56d2e254aac0d4ae4c691db3dd477
Hanwe Lee <mobiledev@kakao.com>
2024-01-28 17:30:46 +0900

    feat: 앱 빌드되도록 수정

---
+++ Project/Projects/App/Sources/App.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoArchiveApp.swift
+//  PhotoArchive
+//
+//  Created by hanwe on 1/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoArchiveApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ Project/Projects/App/Sources/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhotoArchive
+//
+//  Created by hanwe on 1/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
