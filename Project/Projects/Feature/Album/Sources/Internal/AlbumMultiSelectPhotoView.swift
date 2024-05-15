e23c87ab33e084cfd8cb58d4ec3f64d934b27cb7
Hanwe Lee <mobiledev@kakao.com>
2024-05-15 18:59:46 +0900

    feat: [WIP] 사진선택 - 사진선택 화면 구성

--- Project/Projects/Feature/Album/Sources/Internal/AlbumMultiSelectPhotoView.swift
+++ Project/Projects/Feature/Album/Sources/Internal/AlbumMultiSelectPhotoView.swift
@@ -24,6 +24,7 @@ struct AlbumMultiSelectPhotoView: View {
   private let album: Album
   private let itemInset: CGFloat = 3
   private let maxSelectableCount: UInt
+  private let selectedItemWidthHeight: CGFloat = 24
   
   // MARK: - public properties
   
@@ -37,17 +38,15 @@ struct AlbumMultiSelectPhotoView: View {
       ZStack {
         ATBackgroundView()
           .edgesIgnoringSafeArea(.all)
-        GeometryReader { geometry in
-          ScrollView {
-            let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: self.itemInset), count: 3)
-            LazyVGrid(columns: columns, spacing: self.itemInset) {
-              ForEach(0..<self.album.fetchResult.count, id: \.self) { index in
-                if let asset: PHAsset = self.album.fetchResult.object(at: index) as? PHAsset {
-                  ThumbnailView(
-                    asset: asset, 
-                    index: UInt(index)
-                  )
-                }
+        ScrollView {
+          let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: self.itemInset), count: 3)
+          LazyVGrid(columns: columns, spacing: self.itemInset) {
+            ForEach(0..<self.album.fetchResult.count, id: \.self) { index in
+              if let asset: PHAsset = self.album.fetchResult.object(at: index) as? PHAsset {
+                ThumbnailView(
+                  asset: asset,
+                  index: UInt(index)
+                )
               }
             }
           }
@@ -93,12 +92,61 @@ struct AlbumMultiSelectPhotoView: View {
               .scaledToFill()
               .aspectRatio(contentMode: .fill)
               .clipped()
+            FocusBoxView(asset: asset)
+            SelectBoxView(asset: asset)
           }
         }
         .frame(maxHeight: .infinity)
       })
     }
     
+  }
+  
+  @ViewBuilder
+  private func SelectBoxView(asset: PHAsset) -> some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      ZStack(alignment: .topTrailing) {
+        Rectangle()
+          .fill(.clear)
+          .border(viewStore.selectedAssetList.contains(asset) ? Gen.Colors.purple.color : .clear, width: 2)
+        if viewStore.selectedAssetList.contains(asset) {
+          SelectedItemNumberView(value: 1)
+            .padding(8)
+        } else {
+          UnselectedItemNumberView()
+            .frame(alignment: .topTrailing)
+            .padding(8)
+        }
+      }
+    }
+  }
+  
+  @ViewBuilder
+  private func SelectedItemNumberView(value: UInt) -> some View {
+    ZStack {
+      Circle()
+        .fill(Gen.Colors.purple.color)
+      Text("\(value)")
+        .font(.fonts(.body14))
+        .foregroundStyle(Gen.Colors.white.color)
+    }
+    .frame(width: self.selectedItemWidthHeight, height: self.selectedItemWidthHeight)
+  }
+  
+  @ViewBuilder
+  private func UnselectedItemNumberView() -> some View {
+    Circle()
+      .fill(Gen.Colors.white.color.opacity(0.8))
+      .stroke(Gen.Colors.gray300.color, lineWidth: 2)
+      .frame(width: self.selectedItemWidthHeight, height: self.selectedItemWidthHeight)
+      .clipShape(.circle)
+  }
+  
+  @ViewBuilder
+  private func FocusBoxView(asset: PHAsset) -> some View {
+    WithViewStore(store, observe: { $0 }) { viewStore in
+      viewStore.selectedAssetList.contains(asset) ? Gen.Colors.gray200.color.opacity(0.3) : .clear
+    }
   }
   
   // MARK: - internal method
@@ -114,6 +162,6 @@ struct AlbumMultiSelectPhotoView: View {
 //      print("selected: \(selected)")
 //    })
 //  }
-//  
+//
 //}
 
