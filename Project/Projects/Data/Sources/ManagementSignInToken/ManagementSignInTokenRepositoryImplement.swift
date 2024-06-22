65ca1dae029c2f6cf77dbf27d45c06e4657a26f7
Hanwe Lee <mobiledev@kakao.com>
2024-06-22 21:03:44 +0900

    feat: 막 추가한 각각의 유즈케이스의 데이터 레이어 추가

---
+++ Project/Projects/Data/Sources/ManagementSignInToken/ManagementSignInTokenRepositoryImplement.swift
@@ -0,0 +1,34 @@
+//
+//  ManagementSignInTokenRepositoryImplement.swift
+//  Data
+//
+//  Created by hanwe on 6/22/24.
+//  Copyright © 2024 TeamArchive. All rights reserved.
+//
+
+import Domain
+import ArchiveFoundation
+
+public final class ManagementSignInTokenRepositoryImplement: ManagementSignInTokenRepository {
+  
+  
+  // MARK: - private properties
+  
+  // MARK: - internal properties
+  
+  // MARK: - life cycle
+  
+  public init() {
+    
+  }
+  
+  // MARK: - private method
+  
+  // MARK: - internal method
+  
+  public func refreshSignInToken(refreshToken: String) async -> Result<SignInToken, ArchiveError> {
+    // TODO: 개발
+    return .failure(.init(.commonError))
+  }
+
+}
