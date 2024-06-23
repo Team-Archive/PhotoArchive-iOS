9469abb8b74bb1793283e8a4daffbd55d60687ee
JinYoung <kjjy98@naver.com>
2024-06-23 21:44:01 +0900

    feat: CalendarView 구현중

---
+++ Project/Projects/Domain/Sources/Usecase/Calendar/CalendarUsecase.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarUsecase.swift
+//  Domain
+//
+//  Created by jinyoung on 6/11/24.
+//  Copyright © 2024 TeamArchive. All rights reserved.
+//
+
+import Foundation
---
+++ Project/Projects/Feature/Calendar/Sources/Public/CalendarView.swift
@@ -0,0 +1,113 @@
+//
+//  CalendarView.swift
+//  Calendar
+//
+//  Created by jinyoung on 6/11/24.
+//  Copyright © 2024 TeamArchive. All rights reserved.
+//
+
+import SwiftUI
+import ComposableArchitecture
+import UIComponents
+
+public struct CalendarView: View {
+  
+  // MARK: - public state
+  
+  // MARK: - private properties
+  @Bindable private var store: StoreOf<CalendarReducer>
+  
+  // MARK: - public properties
+  
+  
+  
+  // MARK: - life cycle
+  
+  public var body: some View {
+    WithViewStore(store, observe: { $0 }) { state in
+      ZStack {
+        VStack {
+          CalendarMonthInfoView()
+          CalendarWeekDayView()
+          CalendarDayView()
+        }
+      }
+    }
+  }
+  
+  public init(reducer: CalendarReducer) {
+    self.store = .init(initialState: .init(), reducer: {
+      return reducer
+    })
+    
+    self.store.send(.makeDatasource(Date()))
+  }
+  
+  // MARK: - private method
+  @ViewBuilder
+  private func CalendarMonthInfoView() -> some View {
+    WithViewStore(store, observe: { $0 }) { state in
+      HStack(alignment: .center) {
+        Button(action: {
+          state.send(.selectPreviousMonth)
+        }, label: {
+          Gen.Images.arrowLeft24.image
+        })
+        
+        Text("\(state.selectedMonth.formatToyyyyM())")
+          .font(.fonts(.title20))
+          .foregroundStyle(Gen.Colors.white.color)
+          .padding(.horizontal, 12)
+        
+        Button(action: {
+          state.send(.selectNextMonth)
+        }, label: {
+          Gen.Images.arrowRight24.image
+        })
+      }
+      .frame(maxWidth: .infinity, maxHeight: 24)
+      .padding(.bottom, 20)
+    }
+  }
+  
+  @ViewBuilder
+  private func CalendarWeekDayView() -> some View {
+    WithViewStore(store, observe: { $0 }) { state in
+      LazyVGrid(columns: Array(repeating: GridItem(.flexible(minimum: 40, maximum: 60), spacing: 8), count: 7)) {
+        ForEach(state.weekDayList, id: \.self) { day in
+          Text(day)
+            .font(.fonts(.bodyBold14))
+            .foregroundStyle(Gen.Colors.gray300.color)
+            .frame(width: 40)
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: 20, alignment: .center)
+      .padding(.bottom, 8)
+    }
+  }
+  
+  @ViewBuilder
+  private func CalendarDayView() -> some View {
+    WithViewStore(store, observe: { $0 }) { state in
+      LazyVGrid(columns: Array(repeating: GridItem(.flexible(minimum: 40, maximum: 60), spacing: 8), count: 7), spacing: 8) {
+          
+          ForEach(state.datasource.indices, id: \.self) { index in
+            if let day = state.datasource[safe: index]?.day, day != "" {
+              Text(day)
+                .font(.fonts(.bodyBold14))
+                .foregroundStyle(Gen.Colors.white.color)
+                .frame(width: 40, height: 40, alignment: .center)
+                .background(Color.gray.opacity(0.1))
+                .cornerRadius(12)
+            } else {
+              Color.clear
+                .frame(width: 40, height: 40, alignment: .center)
+            }
+          }
+        }
+    }
+  }
+  
+  // MARK: - internal method
+  
+}
